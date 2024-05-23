6370ec279f8dbc4d12957d12adbb32f930c7a9af
KalariyaMihir <mihirkalariya7126@gmail.com>
2024-05-23 15:08:21 +0530

    Advance Python Assesment

--- Assessment/Advance Python Assessment/RegisterForm.py
+++ Assessment/Advance Python Assessment/RegisterForm.py
@@ -3,69 +3,349 @@
 import tkinter
 from tkinter import ttk,messagebox
 
-tk = tkinter.Tk()
-tk.title('Register Form')
-tk.geometry('500x600')
-tk.config(bg='lightgray')
+class Ui:
+    
+    firstName_Label = None
+    firstName_input = None
+    contact_label = None
+    contact_input = None
+    email_label = None
+    email_input = None
+    gender = None
+    gender_male_input = None
+    city_label = None
+    city_list = None
+    role_label = None
+    role = None
+    tk = None
+    
+    product_input = None
+    price_input = None
+    quantity_input = None
+    manager_id_input = None
 
-tag = tkinter.Label(text=' Please Enter Details Below ',bg='orange',fg='white',font='Lucida 10 bold',width=63)
-tag.grid(row=0,column=0)
+    login_name_input = None
+    login_number_input = None
+    
+    update_id = None
+    
+    delete_id = None
+    delete_item = None  
+    
+    
+    buy_name = None
+    buy_quantity = None    
+      
+    # user/Admin Login form
+    def login(self):
+        
+        lg = tkinter.Tk()
+        lg.title('Login')
+        lg.geometry('300x300')
+        lg.config(bg='lightgray')
 
-# Enter Name
-firstName_Label = tkinter.Label(text='Name *',bg='lightgray',fg='black',font='Lucida 10') 
-firstName_Label.place(x=30,y=40)
+        user_label = tkinter.Label(text='Enter User Name ',bg='lightgray',fg='black',font='lucida 10')
+        user_label.place(x=100,y=50)
+        
+        self.login_name_input = tkinter.Entry()
+        self.login_name_input.place(x=90,y=80)
+        
+        login_number_label = tkinter.Label(text='Enter Phone Number',bg='lightgray',fg='black',font='lucida 10')
+        login_number_label.place(x=90,y=130)
+        
+        def login_submit():
+            self.login_name_input = self.login_name_input.get()
+            self.login_number_input = self.login_number_input.get()
+            
+            messagebox.showinfo('Welcome','Login Successful')
+        
+        self.login_number_input = tkinter.Entry()
+        self.login_number_input.place(x=90,y=160)
 
-firstName_input = tkinter.Entry()
-firstName_input.place(x=150,y=43)
+        log_in_btn = tkinter.Button(text='Log in',bg='orange',fg='black',font='Lucida 10',width=10,command = lambda:[login_submit(),lg.destroy()]) 
+        log_in_btn.place(x=110,y=230)
+        
+        lg.mainloop()
 
+    
+    # -------------------------------------------------------------------
+    
+    
+    # user/admin Registration form
+    def reform(self):
+        self.tk = tkinter.Tk()
+        self.tk.title('Register Form')
+        self.tk.geometry('500x600')
+        self.tk.config(bg='lightgray')
 
+        tag = tkinter.Label(text=' Please Enter Details Below ',bg='orange',fg='white',font='Lucida 10 bold',width=63)
+        tag.grid(row=0,column=0)
 
-# Enter Contact Number
-contact_label = tkinter.Label(text='Contact *',bg='lightgray',fg='black',font='Lucida 10')
-contact_label.place(x=30,y=70)
+        # Enter Name
+        self.firstName_Label = tkinter.Label(text='Name *',bg='lightgray',fg='black',font='Lucida 10') 
+        self.firstName_Label.place(x=30,y=40)
 
-
-contact_input = tkinter.Entry()
-contact_input.place(x=150,y=73)
-
-
-# Enter Email Address
-email_label = tkinter.Label(text='Email *',bg='lightgray',fg='black',font='Lucida 10') 
-email_label.place(x=30,y=100)
-
-
-email_input = tkinter.Entry()
-email_input.place(x=150,y=103)
-
-
-# Gender 4
-gender_label = tkinter.Label(text='Gender',bg='lightgray',fg='black',font='Lucida 10')
-gender_label.place(x=30,y=130)
-
-
-gender_male_input = tkinter.Radiobutton(value=0,text='Male',bg='lightgray',fg='Black',font='Lucida 10')
-gender_male_input.place(x=150,y=133)
-
-
-gender_female_input = tkinter.Radiobutton(value=1,text='Female',bg='lightgray',fg='black',font='Lucida 10')
-gender_female_input.place(x=250,y=133)
-
-
-# city
-city_label = tkinter.Label(text='Select Your City',bg='lightgray',fg='black',font='Lucida 10')
-city_label.place(x=30,y=160)
-
-city = ['Rajkot','Ahmadabad','Baroda','Surat','Junagadh','Gandhinagar']
-city_list = ttk.Combobox(values=city)
-city_list.place(x=150,y=163)
-
-# role
-role_label = tkinter.Label(text='Select Your Role *',bg='lightgray',fg='black',font='Lucida 10')
-role_label.place(x=30,y=190)
-
-role = ['Product Manager','Customer']
-role = ttk.Combobox(values=role)
-role.place(x=150,y=193)
+        self.firstName_input = tkinter.Entry()
+        self.firstName_input.place(x=150,y=43)
 
 
-tk.mainloop()
+
+        # Enter Contact Number
+        self.contact_label = tkinter.Label(text='Contact *',bg='lightgray',fg='black',font='Lucida 10')
+        self.contact_label.place(x=30,y=70)
+
+
+        self.contact_input = tkinter.Entry()
+        self.contact_input.place(x=150,y=73)
+
+
+        # Enter Email Address
+        self.email_label = tkinter.Label(text='Email *',bg='lightgray',fg='black',font='Lucida 10') 
+        self.email_label.place(x=30,y=100)
+
+
+        self.email_input = tkinter.Entry()
+        self.email_input.place(x=150,y=103)
+
+
+        # Gender 4
+        gender_label = tkinter.Label(text='Gender',bg='lightgray',fg='black',font='Lucida 10')
+        gender_label.place(x=30,y=130)
+
+
+        self.gender_male_input = tkinter.Radiobutton(value=0,text='Male',bg='lightgray',fg='Black',font='Lucida 10')
+        self.gender_male_input.place(x=150,y=133)
+
+
+        gender_female_input = tkinter.Radiobutton(value=1,text='Female',bg='lightgray',fg='black',font='Lucida 10')
+        gender_female_input.place(x=250,y=133)
+
+
+        # city
+        self.city_label = tkinter.Label(text='Select Your City',bg='lightgray',fg='black',font='Lucida 10')
+        self.city_label.place(x=30,y=160)
+
+        city = ['Rajkot','Ahmadabad','Baroda','Surat','Junagadh','Gandhinagar']
+        self.city_list = ttk.Combobox(values=city)
+        self.city_list.place(x=150,y=163)
+
+        # role
+        self.role_label = tkinter.Label(text='Select Your Role *',bg='lightgray',fg='black',font='Lucida 10')
+        self.role_label.place(x=30,y=190)
+
+        role_list = ['Product Manager','Customer']
+        self.role = ttk.Combobox(values=role_list)
+        self.role.place(x=150,y=193)
+
+
+        # register button
+
+        def btn_click():
+
+            if(len(self.firstName_input.get()) == 0 ):
+                error = tkinter.Label(text='Fill the details where *',bg='lightgray',fg='red',font='Lucida 10')
+                error.place(x=190,y=250)
+
+            else:
+            
+                
+                    
+                messagebox.showinfo(f'Welcome','Your information has been registered')
+                self.firstName_input =  self.firstName_input.get()
+                self.contact_input = self.contact_input.get()
+                self.email_input = self.email_input.get()
+                self.city_list = self.city_list.get() 
+                self.role = self.role.get()
+                
+
+        reg_btn = tkinter.Button(text='Register',bg='Orange',fg='black',font='Lucida 10',width=20, command=lambda:[btn_click(),self.tk.destroy()] )
+        reg_btn.place(x=170,y=300)
+        
+        login_btn = tkinter.Button(text='Login',bg='Orange',fg='black',font='Lucida 10',width=20,command=lambda:[self.tk.destroy(),self.login()])
+        login_btn.place(x=170,y=350)
+
+        self.tk.mainloop()
+        
+        
+    # -------------------------------------------------------------------
+        
+    # product add form
+    def products(self):
+        pr = tkinter.Tk()
+        pr.title('Add Product')
+        pr.geometry('300x200')
+        pr.config(bg='lightgray')
+        
+        
+        product_label = tkinter.Label(text='Product Name :',bg='lightgray',fg='black',font='Lucida 10')
+        product_label.place(x=25,y=30)
+        
+        self.product_input = tkinter.Entry()
+        self.product_input.place(x=140,y=33) 
+        
+        price_label = tkinter.Label(text='Product Price :',bg='lightgray',fg='black',font='Lucida 10')
+        price_label.place(x=25,y=60)
+        
+        self.price_input =tkinter.Entry()
+        self.price_input.place(x=140,y=63)      
+        
+        quantity_label = tkinter.Label(text='Product Quantity :',bg='lightgray',fg='black',font='Lucida 10')
+        quantity_label.place(x=25,y=90)
+        
+        self.quantity_input =tkinter.Entry()
+        self.quantity_input.place(x=140,y=93)    
+        
+        # manager_id_label = tkinter.Label(text="Your Id : ",bg='lightgray',fg='black',font='Lucida 10')  
+        # manager_id_label.place(x=25,y=120)
+        
+        # self.manager_id_input = tkinter.Entry()
+        # self.manager_id_input.place(x=140,y=123)
+        
+        
+        def add_item():
+            
+            self.product_input = self.product_input.get()
+            self.price_input = self.price_input.get()
+            self.quantity_input = self.quantity_input.get()
+            # self.manager_id_input = self.manager_id_input.get()
+            
+            messagebox.showinfo('Welcome','Item has been added') 
+            
+            
+        
+        reg_btn = tkinter.Button(text='Add Item',bg='Orange',fg='black',font='Lucida 10',width=15,command = lambda:[
+           add_item(),pr.destroy()] )
+        reg_btn.place(x=85,y=140)
+
+
+        pr.mainloop()
+    
+    
+    
+    # -------------------------------------------------------------------
+    
+    
+    # update product form
+    def update(self):
+        up = tkinter.Tk()
+        up.title('Update Item')
+        up.geometry('300x200')
+        up.config(bg='lightgray')
+        
+        product_label = tkinter.Label(text='Product Name :',bg='lightgray',fg='black',font='Lucida 10')
+        product_label.place(x=25,y=30)
+        
+        self.product_input = tkinter.Entry()
+        self.product_input.place(x=140,y=33) 
+        
+        price_label = tkinter.Label(text='Product Price :',bg='lightgray',fg='black',font='Lucida 10')
+        price_label.place(x=25,y=60)
+        
+        self.price_input =tkinter.Entry()
+        self.price_input.place(x=140,y=63)      
+        
+        quantity_label = tkinter.Label(text='Product Quantity :',bg='lightgray',fg='black',font='Lucida 10')
+        quantity_label.place(x=25,y=90)
+        
+        self.quantity_input =tkinter.Entry()
+        self.quantity_input.place(x=140,y=93)    
+        
+        update_id_label = tkinter.Label(text="Your Id : ",bg='lightgray',fg='black',font='Lucida 10')  
+        update_id_label.place(x=25,y=120)
+        
+        self.update_id = tkinter.Entry()
+        self.update_id.place(x=140,y=123)
+        
+        def update_item():
+            
+            self.product_input = self.product_input.get()
+            self.price_input = self.price_input.get()
+            self.quantity_input = self.quantity_input.get()
+            self.update_id = self.update_id.get()
+            
+            messagebox.showinfo('Welcome','Item has been UPDATED') 
+            
+            
+        
+        reg_btn = tkinter.Button(text='Update Item',bg='Orange',fg='black',font='Lucida 10',width=15,command = lambda:[
+           update_item(),up.destroy()] )
+        reg_btn.place(x=85,y=160)
+        
+        
+        up.mainloop()
+    
+
+    # -------------------------------------------------------------------
+    
+    # delete item form
+    def delete(self):
+        
+        dl = tkinter.Tk()
+        dl.title('Delete Item')
+        dl.geometry('250x200')
+        dl.config(bg='lightgray')
+        
+        delete_id_label = tkinter.Label(text='Your Id :',bg='lightgray',fg='black',font='Lucida 10')
+        delete_id_label.place(x=20,y=50)
+        
+        self.delete_id = tkinter.Entry()
+        self.delete_id.place(x=100,y=50)
+        
+        delete_item_label = tkinter.Label(text='Item Name :',bg='lightgray',fg='black',font='Lucida 10')
+        delete_item_label.place(x=20, y=80)
+        
+        self.delete_item = tkinter.Entry()
+        self.delete_item.place(x=100,y=80)
+        
+        def confirm():
+            self.delete_id = self.delete_id.get()
+            self.delete_item = self.delete_item.get()
+
+            messagebox.showinfo('warning','Item Has Been Deleted')
+        
+        del_button = tkinter.Button(text='Delete',bg='orange',fg='black',font='Lucida 10',width=10,command = lambda:[confirm(),dl.destroy()])
+        del_button.place(x=80,y=140)
+
+        dl.mainloop()
+        
+        
+    # -------------------------------------------------------------------
+    
+    
+    def buy(self):
+        
+        by = tkinter.Tk()
+        by.title('Shop')
+        by.geometry('300x400')
+        by.config(bg='lightgray')
+        
+        buy_name_label = tkinter.Label(text='Item Name :',bg='lightgray',fg='black',font='Lucida 10')
+        buy_name_label.place(x=30,y=30)         
+        self.buy_name = tkinter.Entry()       
+        self.buy_name.place(x=130,y=33)
+        
+        buy_quantity_label = tkinter.Label(text='Quantity :',bg='lightgray',fg='black',font='Lucida 10')
+        buy_quantity_label.place(x=30,y=60) 
+        self.buy_quantity = tkinter.Entry()
+        self.buy_quantity.place(x=130,y=63)
+        
+        def buy_btn():
+            
+            self.buy_name = self.buy_name.get()
+            self.buy_quantity = self.buy_quantity.get()
+            
+            messagebox.showinfo('welcome','Order has been placed')
+            
+        order_btn = tkinter.Button(text='Order',bg='orange',fg='black',command=lambda:[buy_btn(),by.destroy()],width=15)
+        order_btn.place(x=90,y=100) 
+        
+        by.mainloop()
+
+    
+call = Ui()
+
+# call.reform()
+# call.login()
+# call.update()
+# call.products()
+# call.delete()()
---
+++ Assessment/Advance Python Assessment/db.py
@@ -0,0 +1,292 @@
+# database connectivity page
+
+from RegisterForm import Ui
+# import tkinter
+import pymysql
+
+v = Ui()
+
+class Tables:
+    
+    db = None
+    cr = None
+    v.reform() 
+     
+    try:
+            db = pymysql.connect(host='Localhost',user='root',password='',database='product_management')
+            print('Connection Established')
+            cr = db.cursor()
+    except Exception as e:
+            print(e)
+            
+    # class to create database and tables
+    def Database(self) :
+    
+    
+    
+        # creating table for customer and manger registration
+        try:
+                try:
+                   tbl_customer = 'create table Customer(id integer primary key auto_increment, Name varchar (30), Contact_No varchar (10), Email varchar(50), gender varchar (7), City varchar (20))'
+                   self.cr.execute(tbl_customer) 
+                   self.db.commit()  
+                
+                except Exception as e:
+                    print(e) 
+                        
+                try:
+                   tbl_pm = 'create table ProductManager(id integer primary key auto_increment, Name varchar (30), Contact_No varchar (10), Email varchar(50), City varchar (20))'
+                   self.cr.execute(tbl_pm) 
+                   self.db.commit()
+                
+                except Exception as e:
+                    print(e)
+                        
+        except Exception as e:
+            print(e)
+            
+
+
+        # creating table for customer and manager's  product buying and selling
+        if (v.role == 'Product Manager'):
+
+            try :
+
+                items = 'create table items(id integer primary key auto_increment,productName varchar (30), Price int (6), Quantity int (1))'
+
+                self.cr.execute(items)
+                self.db.commit()
+            
+            
+
+            except Exception as e:
+                print(e)
+                
+        elif(v.role == 'Customer'):
+            
+            try:
+                
+                buy = 'create table buy(id integer primary key auto_increment,ProductName varchar (30),Quantity int (1))'
+                
+                self.cr.execute(buy)
+                self.db.commit()
+            
+            except Exception as e:
+                print(e)
+                
+class Manager(Tables):
+    
+    
+    def addItems(self):
+        
+        try:
+            v.products()
+            add_item = f"insert into items(productName,Price,Quantity) values('{v.product_input}','{v.price_input}','{v.quantity_input}')"
+            
+            self.cr.execute(add_item)
+            self.db.commit()
+           
+            
+        except Exception as e:
+            print(e)
+            
+    def updateItem(self):
+        try :
+            v.update()
+            update_item = f"Update items set productName = '{v.product_input}',Price = '{v.price_input}',Quantity = '{v.quantity_input}' where id = '{v.update_id}'"
+            self.cr.execute(update_item)
+            self.db.commit()
+            
+        except Exception as e:
+            print(e) 
+               
+    def deleteItem(self):
+        
+        try:
+            v.delete_item()
+            delete_item = f"delete from items where id = '{v.delete_id}' and productName = '{v.delete_item}'"
+            self.cr.execute(delete_item)
+            self.db.commit()
+            
+        except Exception as e:
+            print(e)
+            
+
+
+class Customer(Manager):
+    
+    def showItems(self):
+        
+        try:
+            
+            view_item = f"select * from items"
+            self.cr.execute(view_item)
+            show = self.cr.fetchall()
+            
+            print('|\tID \t|\t  ITEM \t\t|\t  PRICE \t|\t QUANTITY \t|\n')
+            
+            for i in show:
+                print(f'|\t {i[0]} \t|\t {i[1]} \t|\t {i[2]} \t\t|\t {i[3]} \t\t|')
+            
+        except Exception as e:
+            print(e)
+
+    def buyItems(self):
+               
+            try:
+                self.showItems()
+                
+                v.buy()
+                buy_item = f"insert into buy(ProductName,Quantity)values('{v.buy_name}','{v.buy_quantity}')"
+
+                view_item = f"select * from items"
+                self.cr.execute(view_item)
+                show = self.cr.fetchall()
+                
+                for i in show:
+
+                    if v.buy_name in i[1]:
+                        
+                        update = f"update items set quantity = '{(i[-1]-1)}' where ProductName = '{v.buy_name}'"
+                        self.cr.execute(update)
+                        self.db.commit()
+                        
+                self.cr.execute(buy_item)
+                self.db.commit()
+                
+            except Exception as e:
+                print(e)
+        
+tb =  Customer()
+tb.Database()
+
+if(v.role == 'Product Manager'):
+    
+    try:
+        
+        p_info = f"insert into ProductManager(Name,Contact_No,Email,City) values('{v.firstName_input}','{int(v.contact_input)}','{v.email_input}','{v.city_list}')"
+
+        tb.cr.execute(p_info)
+        tb.db.commit()
+        
+    except Exception as e:
+        print(e)
+        
+elif(v.role == 'Customer'):
+
+    try:
+        c_info = f"insert into Customer(Name,Contact_No,Email,City) values('{v.firstName_input}','{v.contact_input}','{v.email_input}','{v.city_list}')"
+        
+        tb.cr.execute(c_info)
+        tb.db.commit()
+        
+    except Exception as e:
+        print(e) 
+        
+        
+def LOGIN():
+    view_manager = "select * from ProductManager"
+    tb.cr.execute(view_manager)
+    show_manager = tb.cr.fetchall() 
+    
+    for i in show_manager:
+    
+        if (v.login_name_input == i[1] and v.login_number_input == i[2]):
+            
+            p_c = 'y'
+            
+            while p_c == 'y' or p_c == 'Y':
+            
+                print("""
+                      - Press 1 : Add Item 
+                      - Press 2 : Update Item
+                      - Press 3 : Remove Item
+                      """)
+                choice = int(input("Enter Your Choice : "))
+
+                if choice == 1:
+                    tb.addItems()
+                if choice == 2:
+                    tb.updateItem()
+                if choice == 3:
+                    tb.deleteItem()
+                    
+                p_c = input('Want to Perform more operation (y/n)? : ')
+            
+        else:
+            print("Admin Name or Number is Wrong")
+                
+    view_customer = "select * from Customer"
+    tb.cr.execute(view_customer)
+    show_customer = tb.cr.fetchall()
+
+    for i in show_customer:
+        
+        if(v.login_name_input == i[1] and v.login_number_input == i[2]):
+            
+            c_c = 'y'
+            while c_c == 'y' or c_c == 'Y':
+                print("""
+                        - Press 1 : Show Item
+                        - Press 2 : Buy Item
+                      """)
+
+                c_choice = int(input("enter Choice : "))
+
+                if (c_choice == 1):
+                    tb.showItems()
+                elif(c_choice == 2):
+                    tb.buyItems()
+                c_c = input('Continue shopping (y/n)? : ')
+                
+        else:
+            print("User Name or Number is Wrong")
+
+
+# LOGIN()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# tb.manager()
+
+# tb.addItems()
+# tb.updateItem()
+# tb.delete()
+# tb.customer()
+# tb.buyItems()
+
